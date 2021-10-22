ba3ece824a1221b4471832c805da2a87e71628f5
Ushashree9 <ushashree1999@gmail.com>
2021-10-23 02:33:19 +0530

    add empwagecheck

---
+++ UC-2-empwagecheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+ispresent=1;
+randomcheck=$((RANDOM%2));
+
+if [ $ispresent -eq $randomcheck ]
+then
+      empRatePerHr=20;
+      empHrs=8;
+      salary=$(($empHrs*$empRatePerHr));
+else
+      salary=0;
+fi
